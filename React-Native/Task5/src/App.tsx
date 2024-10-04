eaa83ea4ce05c4aabe9e71e02a63eccfa8f449ce
Fayyaz Khan <fak@pentia.dk>
2024-10-04 14:34:42 +0200

    task 5 update

--- React-Native/Task5/src/App.tsx
+++ React-Native/Task5/src/App.tsx
@@ -1,11 +1,29 @@
 import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Overview from './screens/overview';
 import { TaskProvider } from './contexts/tasks.context';
+import TaskDetails from './screens/taskdetails';
+
+const Stack = createNativeStackNavigator();
 
 const App = () => {
+
+  const scrOptions = { 
+    headerStyle: { backgroundColor: '#6200ee' },
+    headerTitleStyle: { color: 'white'}, 
+    headerBackTitleVisible: false,
+    headerTintColor: 'white'
+  }
+
   return (
     <TaskProvider>
-      <Overview />
+      <NavigationContainer>
+        <Stack.Navigator screenOptions={scrOptions}>
+          <Stack.Screen name="Overview" options={{ headerShown: false}} component={Overview} />
+          <Stack.Screen name="TaskDetails" component={TaskDetails} />
+        </Stack.Navigator>
+      </NavigationContainer>
     </TaskProvider>
   )
 }
